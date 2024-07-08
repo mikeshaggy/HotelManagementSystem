930e574480150c18254f1ce8b33421a2376b1b51
mikeshaggy <michalbagan@icloud.com>
2024-07-08 20:54:07 +0200

    Added BaseRepository

---
+++ HotelManagementSystem/src/main/java/com/mikeshaggy/hms/repository/base/BaseRepository.java
@@ -0,0 +1,8 @@
+package com.mikeshaggy.hms.repository.base;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
+}
